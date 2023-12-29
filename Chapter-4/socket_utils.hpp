3843a52976ddfc32cf28637169d9be5eeb63c874
Sowrya Gali <galisowrya@gmail.com>
2023-12-29 00:42:34 +0000

    Added ttl, naggling, blocking functionalities in sockets

--- Chapter-4/socket_utils.hpp
+++ Chapter-4/socket_utils.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <sys/socket.h>
+#include <netinet/tcp.h>
 #include <netdb.h>
 #include <ifaddrs.h>
+#include <fcntl.h>
 #include <string>
 
 namespace Common {
@@ -19,5 +21,36 @@ namespace Common {
             freeifaddrs(ifaddr);
         }
         return buf;
+    }
+
+    inline auto setNonBlocking(int fd) -> bool {
+        auto flags = fcntl(fd, F_GETFL, 0);
+        if(flags == -1)
+            return false;
+        if(flags & O_NONBLOCK)
+            return true;
+        return (fcntl(fd, F_SETFL, flags | O_NONBLOCK) != -1);
+    }
+    // Disables naggling
+    inline auto setNoDelay(int fd) -> bool {
+        int one = 1;
+        return (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, reinterpret_cast<void*>(&one), sizeof(one)) != -1);
+    }
+
+    inline auto setSOTimestamps(int fd) -> bool {
+        int one = 1;
+        return (setsockopt(fd, SOL_SOCKET, SO_TIMESTAMP, reinterpret_cast<void*>(&one), sizeof(one)) != -1);
+    }
+
+    inline auto wouldBlock() -> bool {
+        return (errno == EWOULDBLOCK || errno == EINPROGRESS);
+    }
+
+    inline auto setTTL(int fd, int ttl) -> bool {
+        return (setsockopt(fd, IPPROTO_IP, IP_TTL, reinterpret_cast<void*>(&ttl), sizeof(ttl)) != -1);
+    }
+
+    inline auto setMcastTTL(int fd, int mcast_ttl) -> bool {
+        return (setsockopt(fd, IPPROTO_IP, IP_MULTICAST_TTL, reinterpret_cast<void*>(&mcast_ttl), sizeof(mcast_ttl)) != -1);
     }
 }
